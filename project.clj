f7b52cf069dea9e797ce3fb34a7358b0e73345a4
Matthieu Sprunck (msprunck) <matthieu@sprunck.com>
2018-10-12 17:22:40 +0200

    Bump to 0.0.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/ring-turnstile-middleware "0.0.4"
+(defproject threatgrid/ring-turnstile-middleware "0.0.5-SNAPSHOT"
   :description "Ring middleware the Turnstile rate limiting service"
   :url "http://github.com/threatgrid/ring-turnstile-middleware"
   :license {:name "Eclipse Public License - v 1.0"
