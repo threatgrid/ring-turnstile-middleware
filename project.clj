6bef68919a0038d55721c44a84c03e7827a26f42
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2020-02-03 10:03:44 -0500

    Bump dependencies, add jdk11 to CI (#11)
    * bump dependencies, add jdk11 to CI
    
    * try openjdk8
    
    * turn on pedantic abort and reflection warnings

--- project.clj
+++ project.clj
@@ -4,13 +4,15 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
+  :pedantic? :abort
   :deploy-repositories [["releases" {:url "https://clojars.org/repo" :creds :gpg}]
                         ["snapshots" {:url "https://clojars.org/repo" :creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.logging "0.4.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/tools.logging "0.5.0"]
                  [threatgrid/turnstile "0.104"]
-                 [metosin/schema-tools "0.10.4"]
-                 [prismatic/schema "1.1.9"]
-                 [ring/ring-mock "0.3.2"]
+                 [metosin/schema-tools "0.12.2"]
+                 [prismatic/schema "1.1.12"]
+                 [ring/ring-mock "0.4.0"]
                  [ch.qos.logback/logback-classic "1.2.3"]]
+  :global-vars {*warn-on-reflection* true}
   :profile {:dev {:dependencies [[com.taoensso/carmine "2.18.1"]]}})
